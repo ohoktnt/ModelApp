48885c320440c57a94844b32916ab0a29f3b866a
Tammy <tammy.n.tran@gmail.com>
2021-04-28 18:03:17 -0400

    added models to project and android app, set up onSelectModel function for button click

--- App.js
+++ App.js
@@ -11,11 +11,50 @@ const deeplab = "Deeplab";
 const posenet = "PoseNet";
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      model: null,
+    }
+  }
+
+  onSelectModel(model) {
+    this.setState({model});
+
+    switch(model) {
+      case ssd:
+        var modelFile = 'models/ssd_mobilenet.tflite';
+        var labelsFile = 'models/ssd_mobilenet.txt';
+        break;
+      case yolo:
+        var modelFile = 'models/yolov2_tiny.tflite';
+        var labelsFile = 'models/yolov2_tiny.txt';
+        break;
+      case deeplab:
+        var modelFile = 'models/deeplabv3_257_mv_gpu.tflite';
+        var labelsFile = 'models/deeplabv3_257_mv_gpu.txt';
+        break;
+      case posenet:
+        var modelFile = 'models/posenet_mv1_075_float_from_checkpoints.tflite';
+        var labelsFile = '';
+        break;
+      default:
+        var modelFile = 'models/mobilenet_v1_1.0_224.tflite';
+        var labelsFile = 'models/mobilenet_v1_1.0_224.txt';
+    }
+  }
+
   render() {
+
 
     var renderButton = (m) => {
       return(
-        <Button title={m} buttonStyle={styles.button}></Button>
+        <Button 
+          title={m} 
+          buttonStyle={styles.button} 
+          onPress={this.onSelectModel.bind(this, m)}>
+        </Button>
       )
     }
 
